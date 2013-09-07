cb9f454d1ce2dd1643866cfb6aef57fa5e3c2d95
Carlos Chinchilla <calufa@gmail.com>
2013-09-07 13:51:12 -0600

    changes

--- core/src/tales/templates/TemplateCommon.java
+++ core/src/tales/templates/TemplateCommon.java
@@ -52,7 +52,7 @@ public abstract class TemplateCommon extends TemplateAbstract{
 				throw new DownloadException(new Throwable(), new Exception(), 404);
 			}
 			
-			//String html = new Download().getURLContentWithCookieAndPost(url, this.getDownloadCookie(), this.getDownloadPost());
+			String html = new Download().getURLContentWithCookieAndPost(url, this.getDownloadCookie(), this.getDownloadPost());
 
 			//Document doc = Jsoup.parse(html);
 
