09c4f4b6feb67b0c9fb00a3ebdfc3965460d17e0
Carlos Chinchilla <calufa@gmail.com>
2013-09-07 21:14:50 -0600

    changes

--- core/src/tales/services/TalesDBHelper.java
+++ core/src/tales/services/TalesDBHelper.java
@@ -20,7 +20,7 @@ public class TalesDBHelper {
 
 
 
-	private static HashMap<String, CopyOnWriteArrayList<String>> pending = new HashMap<String, CopyOnWriteArrayList<String>>();
+	private static HashMap<String, CopyOnWriteArrayList<StringBuilder>> pending = new HashMap<String, CopyOnWriteArrayList<StringBuilder>>();
 	private static HashMap<String, ArrayList<StringBuilder>> all = new HashMap<String, ArrayList<StringBuilder>>();
 
 
@@ -34,7 +34,7 @@ public class TalesDBHelper {
 
 		if(!pending.containsKey(key)){
 
-			pending.put(key, new CopyOnWriteArrayList<String>());
+			pending.put(key, new CopyOnWriteArrayList<StringBuilder>());
 			all.put(key, new ArrayList<StringBuilder>());
 
 			TalesDB talesDB = new TalesDB(config.getThreads(), config.getTemplate().getConnectionMetadata(), config.getTemplateMetadata());
@@ -45,7 +45,7 @@ public class TalesDBHelper {
 
 		if(!all.get(key).contains(documentName)){
 			
-			pending.get(key).add(name);
+			pending.get(key).add(documentName);
 			all.get(key).add(documentName);
 			
 		}
@@ -83,9 +83,9 @@ public class TalesDBHelper {
 
 					Logger.log(new Throwable(), "-cached: " + all.get(key).size() + " -adding: " + pending.get(key).size() + " names to the documents table...");
 
-					for(Iterator<String> it = pending.get(key).iterator(); it.hasNext();){
+					for(Iterator<StringBuilder> it = pending.get(key).iterator(); it.hasNext();){
 
-						String name = it.next();
+						String name = it.next().toString();
 
 						if(!talesDB.documentExists(name)){
 							talesDB.addDocument(name);
