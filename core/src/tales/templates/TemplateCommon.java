5842b7540c60e76248633e7213d0e375729e7ff9
Carlos Chinchilla <calufa@gmail.com>
2013-09-08 03:31:00 -0600

    changes

--- core/src/tales/templates/TemplateCommon.java
+++ core/src/tales/templates/TemplateCommon.java
@@ -128,9 +128,9 @@ public abstract class TemplateCommon extends TemplateAbstract{
 		try{
 
 			for(String link : links){
-				if(!this.getTalesDB().documentExists(link)){
+				//if(!this.getTalesDB().documentExists(link)){
 					TalesDBHelper.queueAddDocumentName(this.getTemplateConfig(), link);
-				}
+				//}
 			}
 
 		} catch (Exception e) {
