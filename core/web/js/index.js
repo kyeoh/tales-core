14021b1b6c554fccebd55db512948efc002ce2f7
Carlos Chinchilla <calufa@gmail.com>
2013-10-22 03:25:27 -0600

    changes

--- core/web/js/index.js
+++ core/web/js/index.js
@@ -40,8 +40,6 @@ $(document).ready(function() {
 			
 				//data = JSON.parse(data.data);
 				data = JSON.parse(data.data);
-				
-				alert(data.length + " - " + JSON.stringify(data));
 		
 				for(var i = 0; i < data.length; i++){
 				
