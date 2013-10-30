437e545f80c484564f01eea921c5a78f8fb06255
Carlos Chinchilla <calufa@gmail.com>
2013-10-29 20:39:27 -0600

    changes

--- core/web/js/index.js
+++ core/web/js/index.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
 		var location = document.location.toString().replace('http://','ws://').replace('https://', 'wss://').replace("#", "");
 		var ws = new WebSocket(location);
 	
-		ws.binaryType = 'blob';
+		ws.binaryType = 'arraybuffer';
 		ws.onopen = function() {
 			
 			$("title").text(companyName);
